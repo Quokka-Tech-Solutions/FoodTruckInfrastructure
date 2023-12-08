3bc68b2e6142363b6a36436466ddd2cd20935206
michaelmcgraw <michael.mcgraw@mykenzie.snhu.edu>
2023-12-08 12:58:51 -0600

    TO-002 Create Subnets

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -11,3 +11,38 @@ resource "aws_vpc" "my-vpc" {
     Name = "Demo VPC"
   }
 }
+# Create Web Public Subnet
+resource "aws_subnet" "web-subnet" {
+  count                   = var.item_count
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = var.web_subnet_cidr[count.index]
+  availability_zone       = var.availability_zone_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "FoodTruckWeb-${count.index}"
+  }
+}
+# Create Application Public Subnet
+resource "aws_subnet" "application-subnet" {
+  count                   = var.item_count
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = var.application_subnet_cidr[count.index]
+  availability_zone       = var.availability_zone_names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "FoodTruckApplication-${count.index}"
+  }
+}
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet" {
+  count             = var.item_count
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = var.database_subnet_cidr[count.index]
+  availability_zone = var.availability_zone_names[count.index]
+
+  tags = {
+    Name = "FoodTruckDatabase-${count.index}"
+  }
+}
