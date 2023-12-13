605fd2bbae2e780b56ebe245fd19aa865df24e2f
michaelmcgraw <michael.mcgraw@mykenzie.snhu.edu>
2023-12-13 12:43:21 -0600

    Created EC2 and Security Groups

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "my-vpc" {
     Name = "Demo VPC"
   }
 }
+
 # Create Web Public Subnet
 resource "aws_subnet" "web-subnet" {
   count                   = var.item_count
@@ -23,6 +24,7 @@ resource "aws_subnet" "web-subnet" {
     Name = "FoodTruckWeb-${count.index}"
   }
 }
+
 # Create Application Public Subnet
 resource "aws_subnet" "application-subnet" {
   count                   = var.item_count
@@ -35,6 +37,7 @@ resource "aws_subnet" "application-subnet" {
     Name = "FoodTruckApplication-${count.index}"
   }
 }
+
 # Create Database Private Subnet
 resource "aws_subnet" "database-subnet" {
   count             = var.item_count
@@ -44,5 +47,156 @@ resource "aws_subnet" "database-subnet" {
 
   tags = {
     Name = "FoodTruckDatabase-${count.index}"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "Demo IGW"
+  }
+}
+
+# Create Web layer route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "WebRT"
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "rt_association" {
+  count          = var.item_count
+  subnet_id      = aws_subnet.web-subnet[count.index].id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+#Create EC2 Instance for Web Subnet
+resource "aws_instance" "webserver" {
+  count                  = var.item_count
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone_names[count.index]
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = aws_subnet.web-subnet[count.index].id
+
+  tags = {
+    Name = "Web Server${count.index}"
+  }
+}
+
+#Create EC2 Instance for Application Subnet
+resource "aws_instance" "appserver" {
+  count                  = var.item_count
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone_names[count.index]
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.application-subnet[count.index].id
+
+  tags = {
+    Name = "App Server${count.index}"
+  }
+}
+
+#Create EC2 Instance for Database Subnet
+resource "aws_instance" "databaseserver" {
+  count                  = var.item_count
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone_names[count.index]
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+  subnet_id              = aws_subnet.database-subnet[count.index].id
+
+  tags = {
+    Name = "Database Server${count.index}"
+  }
+}
+
+# Create Web Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "Web-SG"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-SG"
+  }
+}
+
+# Create Application Security Group
+resource "aws_security_group" "webserver-sg" {
+  name        = "Webserver-SG"
+  description = "Allow inbound traffic from ALB"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description     = "Allow traffic from web layer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Webserver-SG"
+  }
+}
+
+#Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database-SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description     = "Allow traffic from application layer"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webserver-sg.id]
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Database-SG"
   }
 }
